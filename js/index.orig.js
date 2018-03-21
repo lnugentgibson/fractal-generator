b8085872059b7ccee38b371d3ca28c7f55e08a58
Lloyd Nugent Gibson <lng.c9@yandex.com>
2018-03-21 20:46:08 +0000

    added babel

--- js/index.orig.js
+++ js/index.orig.js
@@ -1,5 +1,3 @@
-"use strict";
-
 angular.module("app", []).controller("Ctrl", function () {
   var _this4 = this;
 
--- render.sh
+++ render.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sass scss/style.scss css/style.css
+npx babel babel/index.babel --out-file js/index.js
 haml app.haml app.html && haml index.haml tmp.html
 awk '/###marker###/ { system ( "cat app.html" ) } !/###marker###/ { print; }' tmp.html > index.html
